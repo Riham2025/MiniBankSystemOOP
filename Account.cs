f40c0083019dbca815f0f6c7d925bd426164f757
riham <riham91.alsiyabi@gmail.com>
2025-07-14 16:00:22 +0400

    complate Constructor and defult constructor for Account

--- Account.cs
+++ Account.cs
@@ -8,16 +8,16 @@ namespace MiniBankSystemOOP
 {
     class Account
     {
-        private  static string AccountNumber; // Static variable to hold the account number
-        private static int accountnumberCounter = 0; // Counter for generating unique account numbers
+        public  static string AccountNumber; // Static variable to hold the account number
+        public static int accountnumberCounter = 0; // Counter for generating unique account numbers
 
         public string Name { get; set; } // Property to hold the account name
-        public double Balance { get; private set; } // Property to hold the account balance
+        public double Balance { get; set; } // Property to hold the account balance
         public string NationalID { get; set; } // Property to hold the national ID
         public string phoneNumber { get; set; } // Property to hold the phone number
 
         public string Password { get; set; } // Property to hold the account password
-        public bool IsActive { get; private set; } // Property to indicate if the account is active
+        public bool IsActive { get; set; } // Property to indicate if the account is active
         public bool Accept { get; set; } // Property to indicate if the account is accepted
 
         // Constructor to initialize the account with a name, national ID, phone number, and password
@@ -36,7 +36,9 @@ namespace MiniBankSystemOOP
         // defult constructor
         public Account() {
             NationalID = "0000000000"; // Set a default national ID
-
+            Password = "defaultPassword"; // Set a default password
+            phoneNumber = "0000000000"; // Set a default phone number
+            Balance = 0.0; // Initialize the balance to zero
             Name = "Default Name"; // Set a default name
             IsActive = true; // Set the account as active by default
             Accept = false; // Set the account as not accepted by default
