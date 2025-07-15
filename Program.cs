1d7e7d2502e16171f24d8ab92c60099d2ae80154
riham <riham91.alsiyabi@gmail.com>
2025-07-15 09:32:48 +0400

    User operation

--- Program.cs
+++ Program.cs
@@ -84,6 +84,55 @@
                 }
             }
 
+            // User operation 
+            static void UserOperations()
+            {
+                // Require login first
+                if (currentAccountIndex == -1 && !Login())
+                    return;                     // user failed / aborted
+                bool inUserMenu = true; // to keep the user in the menu until they choose to exit
+                while (inUserMenu)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("\n====== User Menu ======");
+                    Console.WriteLine("1. Deposit");
+                    Console.WriteLine("2. Withdraw");
+                    //Console.WriteLine("3. Check Balance");
+                    //Console.WriteLine("4. Submit Review/Complaint");
+                    //Console.WriteLine("5. Transfer Funds");
+                    //Console.WriteLine("6. Undo Last Complaint");
+                    //Console.WriteLine("7. Request Monthly Statement");
+                    //Console.WriteLine("8. Request Loan");
+                    //Console.WriteLine("9. Show Last N Transactions");
+                    //Console.WriteLine("10. Show Transactions After Date");
+                    Console.WriteLine("0. Back to Main Menu");
+                    Console.Write("Select option: ");
+                    string userChoice = Console.ReadLine();
+
+                    switch (userChoice)
+                    {
+                        case "1": Deposit(); Console.ReadLine(); break;
+                        case "2": Withdraw(); Console.ReadLine(); break;
+                        //case "3": CheckBalance(); Console.ReadLine(); break;
+                        //case "4": submitReview(); Console.ReadLine(); break;
+                        //case "5": TransferFunds(); Console.ReadLine(); break;
+                        //case "6": UndoLastComplaint(); Console.ReadLine(); break;
+                        //case "7": MonthlyStatement(); Console.ReadLine(); break;
+                        //case "8": RequestLoan(); Console.ReadLine(); break;
+                        //case "9": ShowLastNTransactions(); Console.ReadLine(); break;
+                        //case "10": ShowTransactionsAfterDate(); Console.ReadLine(); break;
+
+
+                        case "0":
+                            inUserMenu = false;
+                            Logout();               // end session
+                            break;
+                        default: Console.WriteLine("Invalid choice."); break;
+                    }
+                }
+            }
+
         }
 
         
