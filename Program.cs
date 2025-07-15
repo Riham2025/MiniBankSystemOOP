a3eaeb42cfdd2c945fb9c6e2ca699e4388c2aab7
riham <riham91.alsiyabi@gmail.com>
2025-07-15 09:06:03 +0400

    Sign Up logic

--- Program.cs
+++ Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // List for accounts object 
+            List<Account> accountsList = new List<Account>();
+            // queue for account requests
+            Queue<Account> accountRequests = new Queue<Account>();
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Welcome to Mini Bank System");
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Sign Up");
+                Console.WriteLine("2. Sign In");
+                Console.WriteLine("3. Exit");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        // Sign Up logic
+                        Console.WriteLine("Sign Up selected.");
+                        Console.Write("Enter your name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter your National ID: ");
+                        string nationalID = Console.ReadLine();
+                        Console.Write("Enter your password: ");
+                        string password = Console.ReadLine();
+                        Console.Write("Enter your phone number: ");
+                        string phoneNumber = Console.ReadLine();
+                        Console.Write("Enter account type (e.g., Savings, Current): ");
+                        string type = Console.ReadLine();
+                        SignUp(name, nationalID, password, phoneNumber, type);
+                        Console.WriteLine($"Request Account created successfully! Account Number: {Accounts.GetAccountNumber}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Sign In selected.");
+                        Console.ReadLine(); // Make user hold the screen 
+
+                        break;
+                    case 0: Console.WriteLine("Exiting the system..."); break;
+
+                }
+            }
+
+        public static void SignUp(string name, string nationalID, string password, string phoneNumber, string type)
+        {
+            Accounts newAccount = new Accounts(name, nationalID, password, phoneNumber, type);
+            // add the new account to the accounts list
+
         }
     }
+    
 }
