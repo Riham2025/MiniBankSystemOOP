ebb112cef1b4216318badfef6e5ab31d48072b5f
riham <riham91.alsiyabi@gmail.com>
2025-07-15 09:29:56 +0400

    main menu

--- Program.cs
+++ Program.cs
@@ -11,49 +11,39 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Mini Bank System");
-            Console.WriteLine("Select an option:");
-            Console.WriteLine("1. Sign Up");
-            Console.WriteLine("2. Sign In");
-            Console.WriteLine("3. Exit");
+            // main menu 
+            bool running = true;
 
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
+            while (running)
             {
-                case 1:
-                    // Sign Up logic
-                    Console.WriteLine("Sign Up selected.");
-                    Console.Write("Enter your name: ");
-                    string name = Console.ReadLine();
-                    Console.Write("Enter your National ID: ");
-                    string nationalID = Console.ReadLine();
-                    Console.Write("Enter your password: ");
-                    string password = Console.ReadLine();
-                    Console.Write("Enter your phone number: ");
-                    string phoneNumber = Console.ReadLine();
-                    Console.Write("Enter account type (e.g., Savings, Current): ");
-                    string type = Console.ReadLine();
-                    SignUp(name, nationalID, password, phoneNumber, type);
-                    Console.WriteLine($"Request Account created successfully! Account Number: {Accounts.GetAccountNumber}");
-                    break;
-                case 2:
-                    Console.WriteLine("Sign In selected.");
-                    Console.ReadLine(); // Make user hold the screen 
+                Console.Clear();
+                Console.WriteLine("\n====== SafeBank System Main Menu ======");
+                Console.WriteLine("1. User Menu");
+                Console.WriteLine("2. Admin Menu");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select option: ");
+                string mainChoice = Console.ReadLine();
 
-                    break;
-                case 0: Console.WriteLine("Exiting the system..."); break;
+                switch (mainChoice)
+                {
+                    case "1": UserMenu(); break;
+                    case "2": AdminMenu(); break;
+                    case "0":
+                        SaveAccountInformationToFile();
+                        SaveReviewsToFile();// Save reviews to file before exiting
 
+
+                        running = false;
+                        break;
+                    default: Console.WriteLine("Invalid choice."); break;
+                }
             }
 
-        }
-
-        public static void SignUp(string name, string nationalID, string password, string phoneNumber, string type)
-        {
-            Account newAccount = new Account(name, nationalID, password, phoneNumber, type);
-            // add the new account to the accounts list
+            Console.WriteLine("Thank you for using SafeBank!");
 
         }
+
+        
     }
         
 }
