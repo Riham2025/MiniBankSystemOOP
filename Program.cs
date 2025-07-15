ab921ca24a3ea913c3d910a464252dc1b061ac54
riham <riham91.alsiyabi@gmail.com>
2025-07-15 09:30:15 +0400

    User Menu

--- Program.cs
+++ Program.cs
@@ -41,6 +41,49 @@
 
             Console.WriteLine("Thank you for using SafeBank!");
 
+            // ===========User Menu===========
+            static void UserMenu()
+            {
+                bool inUserMenu = true; // to keep the user in the menu until they choose to exit
+                while (inUserMenu)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("\n====== User Menu ======");
+                    Console.WriteLine("1. Create Account");
+                    Console.WriteLine("2. Login");
+                    Console.WriteLine("0. Back to Main Menu");
+                    Console.Write("Select option: ");
+                    string userChoice = Console.ReadLine();
+
+                    switch (userChoice)
+                    {
+                        case "1": CreateAccount(); Console.ReadLine(); break;
+                        //case "2":                           // Admin Menu
+                        //    if (AdminLogin())               // ← ask for credentials first
+                        //        AdminMenu();                // open menu only on success
+                        //    break;
+
+                        case "2":
+                            Login();
+                            // Require login first
+                            if (currentAccountIndex == -1 && !Login())
+                            { return; }                // user failed / aborted
+                            else
+                            {
+                                // If login is successful, show user operations menu
+                                UserOperations(); // Call the user operations method to show the user menu
+                            }
+                            break;
+                        case "0":
+                            inUserMenu = false;
+                            Logout();               // end session
+                            break;
+                        default: Console.WriteLine("Invalid choice."); break;
+                    }
+                }
+            }
+
         }
 
         
